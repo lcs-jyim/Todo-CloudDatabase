ecb0c58c3930a2598b114923956cdc631a0da643
Josh Yim <jyim@lcs.on.ca>
2024-05-22 21:16:11 -0400

    Added the Supabase framework.

--- TodoList/ViewModel/TodoListViewModel.swift
+++ TodoList/ViewModel/TodoListViewModel.swift
@@ -4,6 +4,14 @@
 //
 //  Created by junxi Yim on 2024/4/18.
 //
+//Database Password:Green5674!@#$%^&*()
+
+//url
+//https://tfvvjwlbjmujaftqssoj.supabase.co
+
+//API Key
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmdnZqd2xiam11amFmdHFzc29qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM0NjQxMjYsImV4cCI6MjAyOTA0MDEyNn0.NoOLBg7SBsuQrnpqZyAcSzf_fND8fp7qg9nq7Ehf0Zs
+
 
 import Foundation
 
