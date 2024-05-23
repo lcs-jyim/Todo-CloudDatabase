b1d2c271274ac6f462de4127d8ffeb492016e90a
Josh Yim <jyim@lcs.on.ca>
2024-05-23 09:52:57 -0400

    Added the ability to search and filter the list of to-do items.

--- TodoList/ViewModel/TodoListViewModel.swift
+++ TodoList/ViewModel/TodoListViewModel.swift
@@ -52,15 +52,39 @@ class TodoListViewModel {
     
     func createToDo(withTitle title: String) {
         
-        // Create the new to-do item instance
-        let todo = TodoItem(
-            title: title,
-            done: false
-        )
-        
-        // Append to the array
-        todos.append(todo)
-        
+        // Create a unit of asynchronous work to add the to-do item
+        Task {
+            
+            // Create the new to-do item instance
+            // NOTE: The id will be nil for now
+            let todo = TodoItem(
+                title: title,
+                done: false
+            )
+            
+            // Write it to the database
+            do {
+                
+                // Insert the new to-do item, and then immediately select
+                // it back out of the database
+                let newlyInsertedItem: TodoItem = try await supabase
+                    .from("todos")
+                    .insert(todo)   // Insert the todo item created locally in memory
+                    .select()       // Select the item just inserted
+                    .single()       // Ensure just one row is returned
+                    .execute()      // Run the query
+                    .value          // Automatically decode the JSON into an instance of TodoItem
+
+                // Finally, insert the to-do item instance we just selected back from the
+                // database into the array used by the view model
+                // NOTE: We do this to obtain the id that is automatically assigned by Supabase
+                //       when the to-do item was inserted into the database table
+                self.todos.append(newlyInsertedItem)
+                
+            } catch {
+                debugPrint(error)
+            }
+        }
     }
     
     func delete(_ todo: TodoItem) {
@@ -102,6 +126,37 @@ class TodoListViewModel {
                     .update(updatedTodo)
                     .eq("id", value: updatedTodo.id!)   // Only update the row whose id
                     .execute()                          // matches that of the to-do being deleted
+                
+            } catch {
+                debugPrint(error)
+            }
+            
+        }
+        
+    }
+    
+    func filterTodos(on searchTerm: String) async throws {
+        
+        if searchTerm.isEmpty {
+            
+            // Get all the to-dos
+            Task {
+                try await getTodos()
+            }
+            
+        } else {
+            
+            // Get a filtered list of to-dos
+            do {
+                let results: [TodoItem] = try await supabase
+                    .from("todos")
+                    .select()
+                    .ilike("title", pattern: "%\(searchTerm)%")
+                    .order("id", ascending: true)
+                    .execute()
+                    .value
+                
+                self.todos = results
                 
             } catch {
                 debugPrint(error)
